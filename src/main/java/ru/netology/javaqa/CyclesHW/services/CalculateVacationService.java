550dfd761ffd41bd72d0ce55e8d793aba58c3004
Natalia Korobchenko <tasha.tviko@gmail.com>
2024-09-09 16:10:36 +0300

    removed extra line

--- src/main/java/ru/netology/javaqa/CyclesHW/services/CalculateVacationService.java
+++ src/main/java/ru/netology/javaqa/CyclesHW/services/CalculateVacationService.java
@@ -4,13 +4,14 @@ public class CalculateVacationService {
     public static int calculateMonthOfVacation(int income, int expenses, int threshold) {
         int vacation = 0; // счётчик месяцев отдыха
         int money = 0; // количество денег на счету
+
         for (int month = 0; month < 12; month++) {
             if (money >= threshold) {
                 // Решаем отдыхать
                 money -= expenses; // Уменьшаем на обязательные траты
                 money /= 3; // Уменьшаем остаток в три раза
                 vacation++; // Увеличиваем счетчик месяцев отдыха
-                money = money - expenses;
+
             } else {
                 // Решаем работать
                 money += income; // Увеличиваем баланс доходом
